50964a2e170fc1abc36717f8060320778aa70036
Avinash <avinash.dindial@mail.utoronto.ca>
2021-10-15 15:33:52 -0400

    Documentation for FifoBook and OrderBook

--- src/FiFoBook.java
+++ src/FiFoBook.java
@@ -17,23 +17,23 @@ public class FiFoBook implements OrderBook {
     }
     @Override
     public Orderable getNextOrder() {
-        if(items == 0){
+        if(items == 0){ //Returns null if order queue is empty
             return null;
         }
-        String nextId = orderQueue.remove();
-        return orders.get(nextId);
+        String nextId = orderQueue.remove(); //Pops the first element in the Queue
+        return orders.get(nextId); //Returns the popped element
     }
 
     @Override
     public Orderable getOrder(String id) {
-        return orders.get(id);
+        return orders.get(id); // Returns the order details, given the order ID
     }
 
     @Override
     public void addOrder(Orderable item) {
         items++;
-        orders.put(item.getOrderId(), item);
-        orderQueue.add(item.getOrderId());
+        orders.put(item.getOrderId(), item); //Adds an item to order hashmap
+        orderQueue.add(item.getOrderId()); //Adds the order to the orderQueue
 
     }
 
@@ -41,15 +41,15 @@ public class FiFoBook implements OrderBook {
     public boolean removeOrder(String id) {
         if(orders.containsKey(id)){
             orders.remove(id);
-            return true;
+            return true; //Returns true once the order is removed from the list of orders
         }
-        return false;
+        return false; //Returns false if the order is not in the list of orders
     }
 
     @Override
     public boolean completeOrder(String id) {
-        if(orders.containsKey(id)){
-            orders.get(id).setOrderStatus(Orderable.COMPLETED);
+        if(orders.containsKey(id)){ //Checks if the order is in the current list of orders
+            orders.get(id).setOrderStatus(Orderable.COMPLETED); //Sets order status to "Completed"
             return true;
         }
         return false;
@@ -58,7 +58,7 @@ public class FiFoBook implements OrderBook {
     @Override
     public boolean setOrderStatus(String id, int status) {
         if(orders.containsKey(id)){
-            orders.get(id).setOrderStatus(status);
+            orders.get(id).setOrderStatus(status); //Sets the status, provided by the user, of an order
             return true;
         }
         return false;
@@ -67,7 +67,8 @@ public class FiFoBook implements OrderBook {
     @Override
     public ArrayList<Sellable> getOrderItems(String id) {
         if(orders.containsKey(id)) {
-            return orders.get(id).getOrderItems();
+            return orders.get(id).getOrderItems(); // Returns a list of items in a specific order.
+                                                    // This order is found using an ID provided by the user
         }
         return null;
     }
@@ -75,8 +76,8 @@ public class FiFoBook implements OrderBook {
     @Override
     public boolean addItemToOrder(String id, Sellable item, int quantity) {
         if(orders.containsKey(id)) {
-            orders.get(id).addItem(item, quantity);
-            return true;
+            orders.get(id).addItem(item, quantity); //Adds items to  a specific order
+            return true;                            //You can add items and the quantity of items to an already existing order
         }
         return false;
     }
@@ -84,7 +85,7 @@ public class FiFoBook implements OrderBook {
     @Override
     public boolean modifyOrder(String id, int index, Sellable item) {
         if(orders.containsKey(id)) {
-            return orders.get(id).modifyOrder(index, item);
+            return orders.get(id).modifyOrder(index, item); //Returns a boolean value to see if an order has been modified
         }
         return false;
     }
@@ -92,7 +93,7 @@ public class FiFoBook implements OrderBook {
     @Override
     public boolean removeItemFromOrder(String id, int index) {
         if(orders.containsKey(id)) {
-            return orders.get(id).removeItem(index);
+            return orders.get(id).removeItem(index); //Removes an item from a given index
         }
         return false;
     }
@@ -101,15 +102,15 @@ public class FiFoBook implements OrderBook {
     @Override
     public int getOrderStatus(String id) {
         if(orders.containsKey(id)) {
-            return orders.get(id).getOrderStatus();
+            return orders.get(id).getOrderStatus(); //Returns the Status of a specific order
         }
-        return -404;
+        return -404; //Returns 404 if ID is not present in the list of orders
     }
 
     @Override
     public float getTotalPriceOfOrder(String id) {
         if(orders.containsKey(id)) {
-            return orders.get(id).getTotalPrice();
+            return orders.get(id).getTotalPrice(); //Returns total price of all items in an order
         }
         return -404;
     }
@@ -117,7 +118,7 @@ public class FiFoBook implements OrderBook {
     @Override
     public boolean isOrderCancelled(String id) {
         if(orders.containsKey(id)) {
-            return orders.get(id).isCancelled();
+            return orders.get(id).isCancelled(); //Returns true if order has been previously cancelled
         }
         System.out.println("NULL POINTER");
         return false;
@@ -126,7 +127,7 @@ public class FiFoBook implements OrderBook {
     @Override
     public boolean setOrderDiscount(String id, float percentage) {
         if(orders.containsKey(id)) {
-            return orders.get(id).setDiscount(percentage);
+            return orders.get(id).setDiscount(percentage); //Sets a specified percentage discount to a specific order
 
         }
         return false;
@@ -135,7 +136,7 @@ public class FiFoBook implements OrderBook {
     @Override
     public float getOrderDiscount(String id) {
         if(orders.containsKey(id)) {
-            return orders.get(id).getDiscount();
+            return orders.get(id).getDiscount(); //Gets the discount percentage of an order, if there is any
 
         }
         return -404;
@@ -144,7 +145,7 @@ public class FiFoBook implements OrderBook {
     @Override
     public boolean updateOrderETA(String id, float time) {
         if(orders.containsKey(id)) {
-            return orders.get(id).updateETA(time);
+            return orders.get(id).updateETA(time); //Updates the estimated time that an order would be completed
 
         }
         return false;
--- src/Interfaces/OrderBook.java
+++ src/Interfaces/OrderBook.java
@@ -8,32 +8,32 @@ public interface OrderBook {
     Orderable getOrder(String id);
 
 
-    void addOrder(Orderable item);
+    void addOrder(Orderable item); //Adds an order to the OrderQueue
 
 
-    boolean removeOrder(String id);
+    boolean removeOrder(String id); //Removes an order from the Queue
 
-    boolean completeOrder(String id);
+    boolean completeOrder(String id); //Updates an order to let the user know it is ready for pickup
 
-    boolean setOrderStatus(String id, int status);
+    boolean setOrderStatus(String id, int status); //Updates the progress of the food item in the Queue
 
-    ArrayList<Sellable> getOrderItems(String id);
+    ArrayList<Sellable> getOrderItems(String id); //Returns a list of items in a particular order
 
-    boolean addItemToOrder(String id, Sellable item, int quantity);
+    boolean addItemToOrder(String id, Sellable item, int quantity); //Adds a menu item to an order
 
-    boolean modifyOrder(String id, int index, Sellable item);
+    boolean modifyOrder(String id, int index, Sellable item); //Changes an order
 
-    boolean removeItemFromOrder(String id, int index);
+    boolean removeItemFromOrder(String id, int index); //Removes a menu item from an order
 
-    int getOrderStatus(String id);
+    int getOrderStatus(String id); //Returns the current status of an order
 
-    float getTotalPriceOfOrder(String id);
+    float getTotalPriceOfOrder(String id); //Returns the cumulative price of all items in an order
 
-    boolean isOrderCancelled(String id);
+    boolean isOrderCancelled(String id); //Returns True if an order is cancelled or not
 
-    boolean setOrderDiscount(String id, float percentage);
+    boolean setOrderDiscount(String id, float percentage); //Sets a specified discount on a specific order
 
-    float getOrderDiscount(String id);
+    float getOrderDiscount(String id); //Returns the discount amount on an order, if any at all
 
-    boolean updateOrderETA(String id, float time);
+    boolean updateOrderETA(String id, float time); //Updates the time remaining for an order to be completed
 }
