55bf68aae54b8acfc3651d289d32345299273758
jeremiahwong007 <73967834+jeremiahwong007@users.noreply.github.com>
2021-10-16 18:49:37 -0400

    Commenting file

--- src/UserAccount.java
+++ src/UserAccount.java
@@ -1,2 +1,6 @@
 public class UserAccount extends Account {
 }
+// The UserAccount class extends to the Account class. This class is responsible for
+// storing user ids, passwords, and the amount of money each user has stored in their account.
+// along with the contructor will be get functions for this class to be used in the Account class.
+
