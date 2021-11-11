bcf4b8132cfbeb95525f42ceef34cd242741c9e2
Avinash <avinash.dindial@mail.utoronto.ca>
2021-11-11 17:56:32 -0500

    Implementation of RegularSingleton

--- src/Entities/RegularSingleton.java
+++ src/Entities/RegularSingleton.java
@@ -1,6 +1,60 @@
 package Entities;
 
+import Entities.Interfaces.IAddon;
 import Entities.Interfaces.ISingleton;
 
+import java.util.List;
+
 public class RegularSingleton{
+    String name;
+    String description;
+    float price;
+    List<IAddon> add_ons;
+    /**
+     * Construct an instance of a RegularFood, which is an object sold by a food truck vendor.
+     *
+     * @param name        Name of the item
+     * @param price       Price of the item
+     * @param description Brief description of the item
+     * @param add_ons   A list of 0 or more add_on entities that make up a Singleton object.
+     */
+    public RegularSingleton(String name, String description, float price, List<IAddon> add_ons){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.add_ons = add_ons;
+    }
+
+    /**
+     * Get the price of this object
+     *
+     * @return Return the price of this object
+     */
+    public float getPrice() {
+        return this.price;
+    }
+    /**
+     * Get the description of this object
+     *
+     * @return Return the description of this object
+     */
+    public String getDescription() {
+        return this.description;
+    }
+    /**
+     * Get the name of this object
+     *
+     * @return Return the name of this object
+     */
+    public String getName() {
+        return this.name;
+    }
+    /**
+     * Get the list of add-ons of this object
+     *
+     * @return Return the list of add-ons of this object
+     */
+    public List<IAddon> getAdd_ons() {
+        return this.add_ons;
+    }
 }
