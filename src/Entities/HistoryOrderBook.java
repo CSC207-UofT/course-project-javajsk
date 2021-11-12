dacba2cdae5db4c585ff9be9ed7af8444d0950eb
Stephanie Lum <stephy.lum87@gmail.com>
2021-11-12 05:17:44 -0500

    Added documentation for IOrderbook and added the method signatures to FifoOrderBook and HistoryOrderBook

--- src/Entities/HistoryOrderBook.java
+++ src/Entities/HistoryOrderBook.java
@@ -1,8 +1,33 @@
 package Entities;
 
+import Entities.Interfaces.IOrder;
 import Entities.Interfaces.IOrderbook;
 
 public class HistoryOrderBook implements IOrderbook {
+    @Override
+    public IOrder getNextOrder() {
+        return null;
+    }
+
+    @Override
+    public IOrder getOrder(int index) {
+        return null;
+    }
+
+    @Override
+    public int getOrderPosition(IOrder order) {
+        return 0;
+    }
+
+    @Override
+    public boolean removeOrder(IOrder order) {
+        return false;
+    }
+
+    @Override
+    public boolean addOrder(IOrder order) {
+        return false;
+    }
     // This is a class to store A **USER'S** order history.
     // This is mainly just a simple storage system but is very similar to an orderbook so i think it makes sense to have
     // this as an Iorderbook
--- src/Entities/Interfaces/IOrderbook.java
+++ src/Entities/Interfaces/IOrderbook.java
@@ -1,13 +1,42 @@
 package Entities.Interfaces;
 
+/**
+ * The Orderbook Interface
+ *
+ * This is an abstract interface that indicates all methods an orderbook should implement
+ */
 public interface IOrderbook {
+    /**
+     * A method that returns the next order in the orderbook
+     * @return next order from orderbook
+     */
     public IOrder getNextOrder();
 
+    /**
+     * A method that returns the order at given index in orderbook
+     * @param index index in orderbook
+     * @return order at given index
+     */
     public IOrder getOrder(int index);
 
+    /**
+     * A method that returns the index of a given order in the orderbook
+     * @param order order that we want the index of
+     * @return index of given order
+     */
     public int getOrderPosition(IOrder order);
 
+    /**
+     * A method that returns whether a given order was successfully removed
+     * @param order order to be removed
+     * @return whether the order was successfully removed
+     */
     public boolean removeOrder(IOrder order);
 
+    /**
+     * A method that returns whether a given order was successfully added
+     * @param order order to be added
+     * @return whether the order was successfully added
+     */
     public boolean addOrder(IOrder order);
 }
