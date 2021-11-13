b4482e7a03cc6bae63e513486a9d8fc74e1975c3
jeremiahwong007 <73967834+jeremiahwong007@users.noreply.github.com>
2021-11-13 02:16:47 -0500

    Added documentation to IFood interface.

--- src/Entities/Interfaces/IFood.java
+++ src/Entities/Interfaces/IFood.java
@@ -2,12 +2,30 @@ package Entities.Interfaces;
 
 import java.util.List;
 
+/**
+ * The Food Interface
+ *
+ * This is an abstract interface that allows for the name, description, price, and components
+ * of a RegularFood object to be accessed using getter methods.
+ */
 public interface IFood {
+    /**
+     *  A method that returns the name of this RegularFood object
+     */
     public String getName();
 
+    /**
+     * A method that returns the description of this RegularFood object
+     */
     public String getDescription();
 
+    /**
+     * A method that returns the price of this RegularFood object
+     */
     public float getPrice();
 
+    /**
+     * A method that returns the components of this RegularFood object
+     */
     public List<ISingleton> getComponents();
 }
