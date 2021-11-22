dee6378c9ec9079e0793b98831103773aa4fb77a
Kathy <kathy.guo@mail.utoronto.ca>
2021-11-21 20:11:43 -0500

    implemented CustomerRepository

---
+++ src/UseCases/DataAcessInterfaces/CustomerRepository.java
@@ -0,0 +1,39 @@
+package UseCases.DataAcessInterfaces;
+
+import Entities.User;
+
+/**
+ * The Customer Interface
+ *
+ * This is an abstract interface for accessing a higher  with customers
+ */
+public interface CustomerRepository {
+
+    /**
+     *  A method for retrieving a user with the given id
+     *
+     * @param id corresponds to the id of the customer
+     * @return a user object that has a matching id
+     */
+    User getCustomer(String id);
+    /**
+     *  A method that changes
+     *
+     * @param id corresponds to the id of the customer
+     * @param customer the new customer object that will replace the existing one
+     * @return true if the change is successful and false if the entry did not change
+     */
+    Boolean setCustomer(String id, User customer);
+    /**
+     *  A method that removes a customer from the repository
+     */
+    Boolean deleteCustomer(String id);
+    /**
+     *  A method for generating an authentication token for the user
+     */
+    String getAuthenticationToken();
+    /**
+     *  A method that checks if a token is still valid
+     */
+    Boolean isValidAuthToken(String token);
+}
