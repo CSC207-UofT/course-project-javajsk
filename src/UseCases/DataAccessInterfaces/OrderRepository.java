f02ccc9f550a6fa7aa2ab8bdd1a5a66b38a3a393
Stephanie Lum <stephy.lum87@gmail.com>
2021-11-21 19:59:11 -0500

    Created and implemented OrderRepository.java - as data access interface

---
+++ src/UseCases/DataAccessInterfaces/OrderRepository.java
@@ -0,0 +1,45 @@
+package UseCases.DataAccessInterfaces;
+
+import Entities.Interfaces.IOrder;
+
+import java.util.ArrayList;
+
+/**
+ * Order
+ * This interface outlines the methods that will be used in accessing data from the higher level data system.
+ */
+public interface OrderRepository {
+
+    /**
+     * Method that gets the order object from higher level data system associated with given Id
+     * @param id Id of desired order
+     * @return order
+     */
+    IOrder getOrder(String id);
+
+    /**
+     * Method returns whether an order was successfully updated in higher level data system
+     * @param id id of order
+     * @param order order
+     * @return whether order was set
+     */
+    boolean setOrder(String id, IOrder order);
+
+    /**
+     * Method sets status of order with given id in the higher level data system
+     * Will return false if order with given id doesn't exist
+     * @param id id of order
+     * @param status status of order
+     * @return whether order status was set
+     */
+    boolean setOrderStatus(String id, String status);
+
+    /**
+     * Method returns all the orders with the given foodtruck id
+     * from the higher level data system
+     *
+     * @param truckId if of foodtruck
+     * @return arraylist of orders
+     */
+    ArrayList<IOrder> getOrdersByFoodTruck(String truckId);
+}
