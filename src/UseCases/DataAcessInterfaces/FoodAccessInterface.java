1805cd2b23f65237b2258cee4c769581304edee1
jeremiahwong007 <73967834+jeremiahwong007@users.noreply.github.com>
2021-11-21 20:08:15 -0500

    Implementation of Food and Vendor access interfaces.

---
+++ src/UseCases/DataAcessInterfaces/FoodAccessInterface.java
@@ -0,0 +1,41 @@
+package UseCases.DataAcessInterfaces;
+
+import Entities.RegularFood;
+
+/**
+ * The FoodAccessInterface Interface
+ *
+ * This is an interface that allows for the attainment, changing, and deletion
+ * of a RegularFood objects in the higher level data storage.
+ */
+public interface FoodAccessInterface {
+        /**
+         *  A method that returns the desired RegularFood object from
+         *  the higher level data storage.
+         *
+         * @param id The associated id of the RegularFood object.
+         * @return Return the RegularFood object with the associated id.
+         */
+        public RegularFood getFood(String id);
+
+        /**
+         *  A method that changes information of the desired RegularFood object in
+         *  the higher level data storage.
+         *
+         * @param id The associated id of the RegularFood object.
+         * @param new_food A new RegularFood object that has the desired new information
+         *
+         * @return Return whether the changes to the RegularFood object have been made.
+         */
+        public Boolean setFood(String id, RegularFood new_food);
+
+        /**
+         *  A method that deletes a desired RegularFood object in
+         *  the higher level data storage.
+         *
+         * @param id The associated id of the RegularFood object.
+         *
+         * @return Return whether the deletion has been made.
+         */
+        public Boolean deleteFood (String id);
+}
---
+++ src/UseCases/DataAcessInterfaces/VendorAccessInterface.java
@@ -0,0 +1,54 @@
+package UseCases.DataAcessInterfaces;
+
+import Entities.Vendor;
+
+/**
+ * The VendorAccessInterface Interface
+ *
+ * This is an interface that allows for the attainment, changing, deletion, and validation
+ * of vendors in the higher level data storage.
+ */
+public interface VendorAccessInterface {
+
+    /**
+     *  A method that returns the desired vendor from
+     *  the higher level data storage.
+     *
+     * @param id The associated id of the vendor.
+     * @return Return the vendor associated with the id.
+     */
+    public Vendor getVendor(String id);
+
+    /**
+     *  A method that changes information of the desired vendor from
+     *  the higher level data storage.
+     *
+     * @param id The associated id of the vendor.
+     * @return Return the vendor associated with the id.
+     */
+    public Boolean setVendor(String id, String password, String name);
+
+    /**
+     *  A method that returns the authentication token used by the vendor
+     *
+     * @param id The associated id of the vendor.
+     * @return Return the authentication token used by the vendor.
+     */
+    public String getAuthenticationToken(String id);
+
+    /**
+     *  A method that returns the id of a vendor from an authentication token.
+     *
+     * @param token The authentication token used by a vendor.
+     * @return Return the associated id of the vendor.
+     */
+    public String getUserIDFromToken(String token);
+
+    /**
+     *  A method that returns whether a vendor's authentication token is currently valid.
+     *
+     * @param token The authentication token used by a vendor.
+     * @return Return whether a vendor's authentication token is currently valid.
+     */
+    public Boolean isTokenValid(String token);
+}
