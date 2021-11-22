acf73b1eb3a0d63015cbc3039de7c77c3dd7cc2d
Akash Ilangovan <akash.ilangovan30@gmail.com>
2021-11-21 20:01:10 -0500

    added FoodTruck Data interface

---
+++ src/UseCases/DataAcessInterfaces/FoodTruckRepository.java
@@ -0,0 +1,22 @@
+package UseCases.DataAcessInterfaces;
+
+import Entities.FoodTruck;
+import Entities.Menu;
+
+public interface FoodTruckRepository {
+
+    /**
+     * @param id The String id associated with the reference of this object in the higher level data system
+     * @return
+     */
+    public FoodTruck getFoodTruck(String id);
+
+    public boolean setFoodTruck(String id, String name, Menu menu, Boolean status);
+
+    public boolean setShopName(String id, String name);
+
+    public boolean setShopMenu(String id, Menu menu);
+
+    public boolean setShopStatus(String id, Boolean status);
+
+}
