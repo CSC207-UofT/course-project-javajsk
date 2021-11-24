abcd6da14d861949993971589c1b9f988abf65c0
Akash Ilangovan <akash.ilangovan30@gmail.com>
2021-11-24 17:21:38 -0500

    FoodTruck usecases

--- src/UseCases/FoodTruck/ChangeMenuUseCase.java
+++ src/UseCases/FoodTruck/ChangeMenuUseCase.java
@@ -7,20 +7,30 @@ import Entities.Menu;
 import UseCases.DataAccessInterfaces.FoodTruckRepository;
 import UseCases.DataAccessInterfaces.VendorRepository;
 import UseCases.OutputBoundary.ErrorPopup;
+import UseCases.OutputBoundary.FoodTruckModel;
 
 public class ChangeMenuUseCase implements ChangeMenuInputBoundary{
     VendorRepository vendorRepository;
     FoodTruckRepository foodTruckRepository;
     ErrorPopup errorDisplayer;
+    FoodTruckModel foodTruckModel;
 
 
     @Override
     public boolean changeMenu(String userToken, String shopID, Menu menu) {
         IVendor vendor = vendorRepository.getUserFromToken(userToken);
-        IShop foodtruck  = foodTruckRepository.getFoodTruck(shopID);
+
         if(vendor != null){
-            foodtruck.setMenu(menu);
-            return foodTruckRepository.save(foodtruck);
+            IShop foodtruck  = foodTruckRepository.getFoodTruck(shopID);
+            if(foodtruck != null){
+                foodtruck.setMenu(menu);
+                foodTruckModel.updateFoodTruck(foodtruck);
+                return foodTruckRepository.save(foodtruck);
+            }
+            else{
+                errorDisplayer.displayError("Invalid ShopID");
+            }
+
         }
         errorDisplayer.displayError("User must be logged in as vendor");
         return false;
--- src/UseCases/FoodTruck/ProcessOrderUseCase.java
+++ src/UseCases/FoodTruck/ProcessOrderUseCase.java
@@ -7,22 +7,29 @@ import Entities.VendorOrderBook;
 import UseCases.DataAccessInterfaces.FoodTruckRepository;
 import UseCases.DataAccessInterfaces.VendorRepository;
 import UseCases.OutputBoundary.ErrorPopup;
+import UseCases.OutputBoundary.FoodTruckModel;
 
 public class ProcessOrderUseCase implements ProcessOrderInputBoundary {
     VendorRepository vendorRepository;
     FoodTruckRepository foodTruckRepository;
     ErrorPopup errorDisplayer;
+    FoodTruckModel foodTruckModel;
 
     @Override
     public Boolean processOrder(String userToken, String shopID) {
         IVendor vendor = vendorRepository.getUserFromToken(userToken);
         if(vendor != null){
             IShop foodtruck = foodTruckRepository.getFoodTruck(shopID);
-            VendorOrderBook orderbook = (VendorOrderBook) foodtruck.getOrderBook(); //TODO: Does this casting make sense?
-            IOrder order = orderbook.getNextOrder();
-            order.setStatus(true); //TODO: We don't have Status codes yet. Its just true and false rn.
-            return foodTruckRepository.save(foodtruck);
-
+            if(foodtruck != null){
+                VendorOrderBook orderbook = (VendorOrderBook) foodtruck.getOrderBook(); //TODO: Does this casting make sense?
+                IOrder order = orderbook.getNextOrder();
+                order.setStatus(true); //TODO: We don't have Status codes yet. Its just true and false rn.
+                foodTruckModel.updateFoodTruck(foodtruck);
+                return foodTruckRepository.save(foodtruck);
+            }
+           else{
+                errorDisplayer.displayError("Invalid ShopID");
+            }
 
         }
         errorDisplayer.displayError("User must be logged in as vendor.");
