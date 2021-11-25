c500ea380d1a4a4b8fe7164b4c59bacab663fc8e
Avinash <avinash.dindial@mail.utoronto.ca>
2021-11-24 19:04:49 -0500

    Implementation of Addon UseCase

--- src/UseCases/Addon/CreateAddonUseCase.java
+++ src/UseCases/Addon/CreateAddonUseCase.java
@@ -3,11 +3,29 @@ package UseCases.Addon;
 import Entities.Interfaces.IAddon;
 import Entities.RegularAddon;
 
+import java.util.Scanner;
+import java.util.UUID;
+
+
 
 public class CreateAddonUseCase implements CreateAddonInputBoundary {
-    @Override
-    public IAddon createAddon(String name, String id, String description, float price) {
-        return new RegularAddon(name, id, description, price);
-
+    public IAddon createAddon(String VendorToken) {
+        Scanner AddonName = new Scanner(System.in);
+        Scanner AddonDescr = new Scanner(System.in);
+        Scanner AddonPrice = new Scanner(System.in);
+        AddonFactory af = new AddonFactory();
+        IVendor vendor = VendorRepository.getAuthenticationToken(VendorToken);
+        if (vendor != null) {
+            String AddonId = UUID.randomUUID().toString();
+            System.out.println("Enter Addon name");
+            String name = AddonName.nextLine();
+            System.out.println("Enter Addon description");
+            String description = AddonDescr.nextLine();
+            System.out.println("Enter Addon Price");
+            float price = AddonPrice.nextFloat();
+            RegularAddon addon = new RegularAddon(name, AddonId, description, price);
+            return AddonRepository.setAddon(AddonId, addon);
+        }
+        return null;
     }
 }
