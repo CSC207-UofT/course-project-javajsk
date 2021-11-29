006572439b2a2fd2d8614ffb490fb10df7f3a607
VijayS02 <48948253+VijayS02@users.noreply.github.com>
2021-11-28 19:23:20 -0500

    Updated Cart - Pushing to Vijay Branch now.

--- src/Adapters/Controllers/CartController.java
+++ src/Adapters/Controllers/CartController.java
@@ -1,16 +1,27 @@
 package Adapters.Controllers;
 
 import Adapters.JSONParser;
+import Entities.Interfaces.IAddon;
+import Entities.Interfaces.ISelection;
+import Entities.Regular.RegularSelection;
 import UseCases.Cart.AddToCartInputBoundary;
 import UseCases.Cart.CreateCartInputBoundary;
 import UseCases.Cart.RemoveFromCartInputBoundary;
+import UseCases.DataAccessInterfaces.AddonRepository;
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 public class CartController {
     AddToCartInputBoundary addToCartInputBoundary;
     CreateCartInputBoundary createCartInputBoundary;
     RemoveFromCartInputBoundary removeFromCartInputBoundary;
     JSONParser parser;
+    AddonRepository addonRepository;
 
     CartController(AddToCartInputBoundary addToCartInputBoundary, CreateCartInputBoundary createCartInputBoundary,
                    RemoveFromCartInputBoundary removeFromCartInputBoundary, JSONParser parser){
@@ -20,11 +31,39 @@ public class CartController {
         this.parser = parser;
     }
 
+    ISelection generateSelection(JSONObject selection){
+        Set<String> addon_ids = selection.keySet();
+        HashMap<IAddon, Integer> finalOrderInfo = new HashMap<>();
+        for(String addon_id: addon_ids){
+            IAddon addon = addonRepository.getAddon(addon_id);
+            finalOrderInfo.put(addon, selection.getInt(addon_id));
+        }
+        return (ISelection) new RegularSelection(finalOrderInfo);
+    }
+
+    List<ISelection> generateOrderInfo(JSONArray selections){
+        ArrayList<ISelection> orderData = new ArrayList<>();
+        for (int i =0; i < selections.length(); i++) {
+            orderData.add(generateSelection(selections.getJSONObject(i)));
+        }
+        return orderData;
+    }
+
     void runAddToCart(String input){
         JSONObject data = this.parser.parse(input);
-        String userToken = (String) data.get("userToken");
+        String userToken = data.getString("userToken");
+        String cartId = data.getString("cartId");
+        String foodId = data.getString("foodId");
+        String shopId = data.getString("shoId");
+        JSONArray selections = data.getJSONArray("orderInfo");
 
-        //this.addToCartInputBoundary.addToCart();
+        List<ISelection> parsedSelections = this.generateOrderInfo(selections);
+
+        this.addToCartInputBoundary.addToCart(cartId, foodId, parsedSelections, shopId, userToken);
+
+    }
+
+    void runCreateCartInputBoundary(){
 
     }
 
