9026548b7befa70025d035d65eeda91e537556ba
Kathy <kathy.guo@mail.utoronto.ca>
2021-11-29 19:08:03 -0500

    updated selection interface and entity

--- src/Entities/Regular/RegularSelection.java
+++ src/Entities/Regular/RegularSelection.java
@@ -9,30 +9,37 @@ import java.util.List;
 public class RegularSelection implements ISelection {
     HashMap<IAddon, Integer> singleSingleton;
 
+    /**
+     * Creates a RegularSelection object
+     * @param selection the addons a customer has chosen for a singleton
+     */
     public RegularSelection(HashMap<IAddon, Integer> selection){
         this.singleSingleton = selection;
     }
 
-    public float getPrice(){
-        float sum = 0;
-        for(IAddon addon : singleSingleton.keySet()){
-            sum += addon.getPrice() * singleSingleton.get(addon);
-        }
-        return sum;
-    }
-
-    @Override
-    public int size() {
+    /**
+     * A method for getting the size of a selection
+     * @return the number of different addons a selection has
+     */
+    public int getSize() {
         return this.singleSingleton.size();
     }
 
+    /**
+     * A method that gets the number of an addon in a selection
+     * @param addon the addon we want the amount for
+     * @return the number of the addon in the selection
+     */
     public Integer get(IAddon addon){
         return singleSingleton.get(addon);
     }
 
-    @Override
+    /**
+     * A method for getting the addons used in a selection
+     * @return a list of the addons used
+     */
     public List<IAddon> getUsedAddons() {
-        return (List<IAddon>) singleSingleton.keySet();
+        return  singleSingleton.keySet().stream().toList();
     }
 
 }
