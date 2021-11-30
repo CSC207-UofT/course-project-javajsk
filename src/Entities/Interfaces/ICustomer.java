d4a1c54108e1f22aa884476b25636258941ec716
Kathy <kathy.guo@mail.utoronto.ca>
2021-11-29 19:06:27 -0500

    updated customer interface and entity

--- src/Entities/Interfaces/ICustomer.java
+++ src/Entities/Interfaces/ICustomer.java
@@ -1,10 +1,28 @@
 package Entities.Interfaces;
 
 public interface ICustomer extends IUser {
+    /**
+     * Returns the id of a customer
+     * @return the id of a customer
+     */
     String getId();
 
+    /**
+     * Method for setting the customer id
+     * @param id the id
+     */
+    void setId(String id);
+
+    /**
+     * A method for setting the cart of a customer
+     * @param cart a cart object
+     */
     void setCart(ICart cart);
 
+    /**
+     * A method for getting the cart of a customer
+     * @return the cart object of a customer
+     */
     ICart getCart();
 
 
--- src/Entities/Regular/RegularCustomer.java
+++ src/Entities/Regular/RegularCustomer.java
@@ -4,30 +4,52 @@ import Entities.Interfaces.ICustomer;
 import Entities.Interfaces.ICart;
 
 public class RegularCustomer implements ICustomer {
-    String ID;
+    String id;
     ICart currentCart;
 
-    public RegularCustomer(String ID,  ICart cartList){
-        this.ID = ID;
+    /**
+     * Creates a RegularCustomer object
+     * @param id the id of the customer
+     * @param cartList the cart of the customer
+     */
+    public RegularCustomer(String id,  ICart cartList){
+        this.id = id;
         this.currentCart = cartList;
     }
+    /**
+     * Returns the id of a customer
+     * @return the id of a customer
+     */
     @Override
     public String getId(){
-        return this.ID;
+        return this.id;
     }
 
+    /**
+     * Method for setting the customer id
+     * @param id the id
+     */
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * A method for setting the cart of a customer
+     * @param cart a cart object
+     */
     @Override
     public void setCart(ICart cart){
         currentCart = cart;
     }
 
+    /**
+     * A method for getting the cart of a customer
+     * @return the cart object of a customer
+     */
     @Override
     public ICart getCart() {
         return currentCart;
     }
 
-    @Override
-    public String getID() {
-        return this.getId();
-    }
 }
