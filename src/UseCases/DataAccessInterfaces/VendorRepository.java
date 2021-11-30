08723a3674d78232103a5cd90f8753312a80cad5
Kathy <75864989+kathybguo@users.noreply.github.com>
2021-11-29 19:42:14 -0500

    Update VendorRepository.java

--- src/UseCases/DataAccessInterfaces/VendorRepository.java
+++ src/UseCases/DataAccessInterfaces/VendorRepository.java
@@ -9,47 +9,26 @@ import Entities.Interfaces.IVendor;
  * of vendors in the higher level data storage.
  */
 public interface VendorRepository extends UserRepository{
-
-    /**
+    
+/**
      *  A method that returns the desired vendor from
      *  the higher level data storage.
      *
      * @param id The associated id of the vendor.
      * @return Return the vendor associated with the id.
      */
-    IVendor getVendor(String id);
+    public IVendor getVendor(String id);
 
     /**
-     *  A method that returns the authentication token used by the vendor
+     *  A method that changes information of the desired vendor from
+     *  the higher level data storage.
      *
      * @param id The associated id of the vendor.
-     * @return Return the authentication token used by the vendor.
+     * @return Return the vendor associated with the id.
      */
-    String getAuthenticationToken(String id);
-
-    /**
-     *  A method that returns the id of a vendor from an authentication token.
-     *
-     * @param token The authentication token used by a vendor.
-     * @return Return the associated id of the vendor.
-     */
-    String getUserIdFromToken(String token);
-
-    /**
-     *  A method that returns the vendor from an authentication token.
-     *
-     * @param token The authentication token used by a vendor.
-     * @return Return the associated vendor.
-     */
-    IVendor getVendorFromToken(String token);
-
-    /**
-     *  A method that returns whether a vendor's authentication token is currently valid.
-     *
-     * @param token The authentication token used by a vendor.
-     * @return Return whether a vendor's authentication token is currently valid.
-     */
-    Boolean isTokenValid(String token);
+    public Boolean setVendor(String id, String password, String name);
+    
+    
     /**
     * Method for saving a vendor into the repository
     * @param vendor the vendor object being saved to the repository
