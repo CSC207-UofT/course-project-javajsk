2cfc80592ee3ffce6dfa6386d8a4203349808dbb
Stephanie Lum <stephy.lum87@gmail.com>
2021-12-04 16:45:57 -0500

    Implemented customer presenter

--- src/main/java/presenters/CustomerPresenter.java
+++ src/main/java/presenters/CustomerPresenter.java
@@ -6,21 +6,40 @@ import businessrules.outputboundaries.ResponseObject;
 /**
  * Presenter for Customer
  */
-public class CustomerPresenter implements CustomerBoundary {
-
-
+public class CustomerPresenter implements CustomerBoundary{
+    /**
+     * A method that returns a responseObject containing information to display the user token
+     *
+     * @param token token of user
+     * @return responseObject containing information to display token
+     */
     @Override
     public ResponseObject displayToken(String token) {
-        return null;
+        return new ResponseObject(200, "", token);
     }
 
+    /**
+     * A method that returns a responseObject containing information to display an error message
+     *
+     * @param message error message to add to response object
+     * @return response object containing information to display error message
+     */
     @Override
     public ResponseObject error(String message) {
-        return null;
+        return new ResponseObject(406, message, null);
+        // 406 is http status code for not acceptable
     }
 
+    /**
+     * A method that returns a responseObject containing information to display an error message due to
+     * unauthorized access
+     *
+     * @param message message to add to response object
+     * @return response object containing information to display message
+     */
     @Override
     public ResponseObject unauthorizedAccess(String message) {
-        return null;
+        return new ResponseObject(401, message, null);
+        // 401 is http status code of unauthorized
     }
 }
