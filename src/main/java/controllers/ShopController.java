9b3f6da0539c61750a6c53261989f47a6d144048
Akash Ilangovan <akash.ilangovan30@gmail.com>
2021-12-04 01:57:05 -0500

    Added constructer, getters, and setters to Response object

--- src/main/java/controllers/ShopController.java
+++ src/main/java/controllers/ShopController.java
@@ -1,4 +1,25 @@
 package main.java.controllers;
 
+import adapters.dam.entityrepoitories.ShopDB;
+import adapters.dam.entityrepoitories.VendorDB;
+import businessrules.outputboundaries.ResponseObject;
+import businessrules.shop.inputboundaries.ChangeShopStatus;
+import businessrules.shop.inputboundaries.ModifyShop;
+import entities.Shop;
+import org.json.JSONObject;
+
 public class ShopController {
+
+    ShopDB shopDB;
+    VendorDB vendorDB;
+    ChangeShopStatus changeShopStatus;
+    ModifyShop modifyShop;
+
+
+    public Shop runChangeShopStatus(String input){
+        JSONObject data = new JSONObject(input);
+        String vendorToken = data.getString("vendorToken");
+        Boolean newStatus = data.getBoolean("newStatus");
+        ResponseObject response = changeShopStatus.changeShopStatus(vendorToken, newStatus);
+    }
 }
