d673fbd0be061e5d6fc186dbf81de90ec019e51d
Akash Ilangovan <akash.ilangovan30@gmail.com>
2021-12-05 12:35:26 -0500

    updated cart controller

--- src/main/java/businessrules/cart/usecases/ViewCartInteractor.java
+++ src/main/java/businessrules/cart/usecases/ViewCartInteractor.java
@@ -12,6 +12,14 @@ public class ViewCartInteractor implements ViewCart {
     ObjectBoundary<Cart> cartObjectBoundary;
     CustomerRepository customerRepository;
     RepositoryBoundary repositoryBoundary;
+
+    public ViewCartInteractor(ObjectBoundary<Cart> cartObjectBoundary, CustomerRepository customerRepository,
+                              RepositoryBoundary repositoryBoundary) {
+        this.cartObjectBoundary = cartObjectBoundary;
+        this.customerRepository = customerRepository;
+        this.repositoryBoundary = repositoryBoundary;
+    }
+
     @Override
     public ResponseObject viewCart(String userToken) {
         Customer customer = (Customer) customerRepository.getUserFromToken(userToken);
