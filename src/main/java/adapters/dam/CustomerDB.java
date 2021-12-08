2c28e81cb079c1843d67b1cb90a26331e6f67fa9
Akash Ilangovan <akash.ilangovan30@gmail.com>
2021-12-08 14:07:18 -0500

    Added Javadoc to CustomerDB

--- src/main/java/adapters/dam/CustomerDB.java
+++ src/main/java/adapters/dam/CustomerDB.java
@@ -11,7 +11,8 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CustomerDB implements CustomerRepository {
+public class
+CustomerDB implements CustomerRepository {
     DBGateway databaseConnector;
     TokenSigner tokenSigner;
     final String tableName = "Customer";
@@ -21,22 +22,40 @@ public class CustomerDB implements CustomerRepository {
         this.databaseConnector = databaseConnector;
     }
 
+    /**
+     * @param id the id of the entry to read from the database
+     * @return the constructed entitity from the database
+     */
     @Override
     public Customer read(String id) {
         return loadCustomerFromJSON(databaseConnector.read(tableName, id));
     }
 
+    /**
+     * @param id   the id of the entry to update
+     * @param item the new object to replace the old object with
+     * @return The status of the update. True if it went through,false if it didn't
+     */
     @Override
     public boolean update(String id, Customer item) {
         return databaseConnector.update(tableName, id, loadJSONFromCustomer(item));
 
     }
 
+    /**
+     * @param item the entity to add to the database, the ID will initially be null
+     * @return A String representation of the object that was created. This will have the ID the database generates for it
+     */
     @Override
     public String create(Customer item) {
         return databaseConnector.create(tableName, loadJSONFromCustomer(item));
     }
 
+    /**
+     * @param parameter the parameter to search by
+     * @param needle    the value of the parameter to find
+     * @return A list of constructed customer entities
+     */
     @Override
     public List<Customer> readMultiple(String parameter, String needle) {
         List<Customer> customerList = new ArrayList<>();
@@ -47,6 +66,11 @@ public class CustomerDB implements CustomerRepository {
         return customerList;
     }
 
+    /**
+     * @param fieldName the field to search by
+     * @param needle    the value of the field to find
+     * @return A constructed customer object
+     */
     @Override
     public Customer findOneByFieldName(String fieldName, String needle) {
         JSONObject customerData = databaseConnector.readOne(tableName,fieldName, needle);
@@ -57,6 +81,10 @@ public class CustomerDB implements CustomerRepository {
     }
 
 
+    /**
+     * @param userToken the user token the user received when logging in
+     * @return A constructed user object linked to that token.
+     */
     @Override
     public User getUserFromToken(String userToken) {
         String info = tokenSigner.getIdFromToken(userToken);
@@ -67,6 +95,11 @@ public class CustomerDB implements CustomerRepository {
         return read(userId);
     }
 
+    /**
+     * @param username the user's username
+     * @param password the user's password
+     * @return Token associated with that user session
+     */
     @Override
     public String authenticateUser(String username, String password) {
         Customer customer = findOneByFieldName("username", username);
@@ -80,6 +113,10 @@ public class CustomerDB implements CustomerRepository {
         return tokenSigner.generateToken(token_parameter);
     }
 
+    /**
+     * @param customer Customer entity that we want to create a JSON of
+     * @return The JSONObject representation of that object
+     */
     public static JSONObject loadJSONFromCustomer(Customer customer){
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("id", customer.getId());
@@ -90,6 +127,10 @@ public class CustomerDB implements CustomerRepository {
     }
 
 
+    /**
+     * @param jsonObject JSON Object representation of Customer
+     * @return A constructed customer object
+     */
     public Customer loadCustomerFromJSON(JSONObject jsonObject){
         CartDB cartLoader = new CartDB(databaseConnector);
         OrderDB orderLoader = new OrderDB(databaseConnector);
