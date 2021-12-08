e6f493e811ba7c7b7a77aec6904d3346dc095b69
Akash Ilangovan <akash.ilangovan30@gmail.com>
2021-12-08 15:08:46 -0500

    Added menu controller javadoc

--- src/main/java/com/javajsk/uoftruck/controllers/MenuController.java
+++ src/main/java/com/javajsk/uoftruck/controllers/MenuController.java
@@ -69,38 +69,72 @@ public class MenuController{
         this.viewMenu =new ViewMenuInteractor(shopRepository, repositoryBoundary, menuObjectBoundary);
     }
 
+    /**
+     * @param vendorToken Token associated with vendor trying to add to their menu
+     * @param addon The addon object they want to add ot their menu
+     * @return A ResponseObject with the menu with the newly added addon, with status codes
+     */
     @PutMapping("/AddAddonToMenu/{vendorToken}")
     public ResponseObject runAddAddontoMenu(@PathVariable String vendorToken, @RequestBody String addon){
         Addon addon1 = addonRepository.loadAddonFromJSON(new JSONObject(addon));
         return addAddonToMenu.addAddon(vendorToken, addon1);
     }
+
+    /**
+     * @param vendorToken Token associated with vendor trying to add to their menu
+     * @param food The food object they want to add ot their menu
+     * @return A ResponseObject with the menu with the newly added food, with status codes
+     */
     @PutMapping("/AddFoodToMenu/{vendorToken}")
     public ResponseObject runAddFoodtoMenu(@PathVariable String vendorToken, @RequestBody String food){
         Food food1 = foodRepository.loadFoodFromJSON(new JSONObject(food));
         return addFoodToMenu.addFood(vendorToken, food1);
     }
+
+    /**
+     * @param shopId The shopid of the shop we want to  check available addons of
+     * @return A ResponseObject with all the available addons of the given shop, with status codes
+     */
     @GetMapping("/GetAvailableAddons/{shopId}")
     public ResponseObject runGetAvailableAddons(@PathVariable String shopId){
         return getAvailableAddons.getAvailableAddons(shopId);
     }
 
+    /**
+     * @param shopId The shopid of the shop we want to  check available foods of
+     * @return A ResponseObject with all the available foods of the given shop, with status codes
+     */
     @GetMapping("/GetAvailableFoods/{shopId}")
     public ResponseObject runGetAvailableFoods(@PathVariable String shopId){
         return getAvailableFoods.getAvailableFoods(shopId);
     }
 
+    /**
+     * @param vendorToken Token associated with vendor trying to remove from their menu
+     * @param addon The addon object they want to remove from their menu
+     * @return A ResponseObject with the menu with the recently removed addon, with status codes
+     */
     @PutMapping("/RemoveAddonFromMenu/{vendorToken}")
     public ResponseObject runRemoveAddonFromMenu(@PathVariable String vendorToken, @RequestBody String addon){
         Addon addon1 = addonRepository.loadAddonFromJSON(new JSONObject(addon));
         return removeAddonFromMenu.removeAddon(vendorToken, addon1);
     }
 
+    /**
+     * @param vendorToken Token associated with vendor trying to remove from their menu
+     * @param food The food object they want to remove from their menu
+     * @return A ResponseObject with the menu with the newly removed food, with status codes
+     */
     @PutMapping("/RemoveFoodFromMenu/{vendorToken}")
     public ResponseObject runRemoveFoodFromMenu(@PathVariable String vendorToken, @RequestBody String food){
         Food food1 = foodRepository.loadFoodFromJSON(new JSONObject(food));
         return removeFoodFromMenu.removeFood(vendorToken, food1);
     }
 
+    /**
+     * @param shopId the shop id of the menu we're trying to view
+     * @return A ResponseObject with the menu of the given shop, with status codes
+     */
     @GetMapping("/ViewMenu/{shopId}")
     public ResponseObject runViewMenu(@PathVariable String shopId){
         return viewMenu.viewMenu(shopId);
